979c0b2fbaa4ac78c7dc0a9ec1c63fb69d352599
BeniKarunia <beni.karunia73@gmail.com>
2022-09-14 02:34:47 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10777/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10777
+
+object TaskList {
+    val taskList = listOf<Task>(
+        Task("Chapter 1","Bangun Tidur", "Bangun setelah istirahat yang cukup dan rapikan tempat tidur"),
+        Task("Chapter 2", "Bersiap Kuliah","Mandi, Masukkan barang ke tas, Memanaskan Motor"),
+        Task("Chapter 3", "Kuliah","Mengikuti sesi perkuliahan sesuai jadwal sampai selesai"),
+        Task("Chapter 4", "Aktivitas lain", "Pulang ke kost, mandi sore, pergi bersama teman atau kerja tugas"),
+        Task("Chapter 5", "Istirahat", "Tidak lupa cuci muka dan sikat gigi lalu tidur untuk hadapi rutinitas yang sama")
+    )
+}
